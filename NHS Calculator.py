bb8029bd175804d08c44652ff4a3492b28f44a96
567-AMBITIONS <dan.turton@ambitionspersonnel.com>
2025-10-23 10:48:08 +0100

    Add files via upload

---
+++ NHS Calculator.py
@@ -0,0 +1,36 @@
+-	# === Streamlit Calculator ===
+
+import streamlit as st
+
+st.set_page_config(page_title="INSERT NAME OF CALCULATOR", layout="wide")
+
+st.image("Ambitions Public Sector Logo.png", width=240)
+st.markdown(
+    "<h1 style='font-size: 40px;'>INSERT NAME OF CALCULATOR</h1>",
+    unsafe_allow_html=True
+)
+
+fixed_number = INSERT SAVINGS PER WORKER
+
+st.markdown(
+    '<p style="font-size:25px; margin: 0; line-height: 1.1;">Enter the quantity of workers:</p>',
+    unsafe_allow_html=True
+)
+
+user_input = st.number_input(
+  "", min_value=1, step=1, format="%d")
+
+result = user_input * fixed_number
+
+formatted_result = f"£{result:,.2f}"
+
+st.markdown(
+    f"""
+    <p style='color:red; font-size:40px; font-weight:bold; text-align:center;'>
+        Annual Savings Achieved: {formatted_result}
+    </p>
+    """,
+    unsafe_allow_html=True
+
+3,172.38
+NHS Calculator
